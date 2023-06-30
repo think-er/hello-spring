bd42d07c85b7293259f9ebb1fbd7165e99632857
thinker <jeonghun5092@gmail.com>
2023-06-30 13:18:07 +0900

    회원 관리 컨트롤러, UI 추가

---
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -0,0 +1,54 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public HomeController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    // 우선순위가 있다.
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    /*
+    * 보통 데이터를 등록할 때는 POST
+    * 조회할 때는 GET
+    * */
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,14 +9,12 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 // 서비스 클래스는 비즈니스에 가까운 용어를 써야 한다. ->
 // 그렇게 해야 개발자든 기획자가 이상하다고 하면 어디 부분 살펴보자고 매핑이 되기 때문이다.
 public class MemberService {
 // Ctrl + Shift + T = Create New Test
     private final MemberRepository memberRepository;
 
-    @Autowired
     // MemberService 입장에서 자신이 직접 new 하지 않는다.
     // 외부에서 MemberRepository를 넣어준다. -> 의존성 주입 (Dependency Injection, DI) 이라고 한다.
     public MemberService(MemberRepository memberRepository) {
